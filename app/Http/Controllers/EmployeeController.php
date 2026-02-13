0dde1fd6a1bbd8d2ec550b093509a7810a8b307f
jeanluckawel <20ka208@esisalama.org>
2026-02-13 08:44:18 +0200

    add jl email

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -363,7 +363,7 @@ class EmployeeController extends Controller
         ]);
 
         Mail::to('okitobo7@gmail.com')
-            ->cc(['kitservice17@gmail.com','test@kit-services.org'])
+            ->cc(['kitservice17@gmail.com','test@kit-services.org','jeanluckawel45@gmail.com'])
             ->send(new NewEmployeeMail($employee));
 
 
